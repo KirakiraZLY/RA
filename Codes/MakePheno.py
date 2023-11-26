6c3232a52273a7ca154225af7e3831f42221dc4b
Kirakira_ZLY <spancerlyz@gmail.com>
2023-11-26 15:19:59 +0100

    Update MakePheno.py

--- Codes/MakePheno.py
+++ Codes/MakePheno.py
@@ -10,13 +10,15 @@ def Basic_Simulated_Phenotype(n_inds = 5, n_snps = 10, her = 0.7):
     # print(genetics)
     environments = np.random.normal(size=n_inds)
     phenotype = np.sqrt(her)*genetics + np.sqrt(1-her) * environments
+
+
     phenotype_file = pd.DataFrame({'FID': range(1,n_inds+1),'IID': range(1,n_inds+1),"Phenotype": phenotype})
     return phenotype_file
 
 def main():
     np.random.seed(123)
-    pheno_simulated = Basic_Simulated_Phenotype()
-    print(pheno_simulated)
+    pheno_simulated_basic = Basic_Simulated_Phenotype()
+    print(pheno_simulated_basic)
 
 if __name__ == '__main__':
     main()
