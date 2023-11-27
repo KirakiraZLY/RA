e15a5685281dd5459b977ffa6313560129d85665
Kirakira_ZLY <spancerlyz@gmail.com>
2023-11-27 15:32:15 +0100

    Update MakePheno.py

--- Codes/MakePheno.py
+++ Codes/MakePheno.py
@@ -2,10 +2,8 @@
 This is a code to simulate phenotypes. It supports to select the number of individuals, the number of genotypes,
 the tau (which represents the h2 in GCTA model, and tau1 in LDAK-thin model). You can choose heritability models
 between GCTA and LDAK-thin by commenting in def Simulated_Phenotype. If you choose the LDAK-thin model, then the
-maf is calculated based on the genotype simulation, and W for LD is simulated randomly between 0 and 1 (Can only
-calculate ld_score_matrix by now).
-
-Question: if maf 1...j corresponds to SNP 1...j, then how were these values calculated?
+maf (1*M) is calculated based on the genotype simulation, and W for LD is simulated randomly between 0 and 1, or a vector
+of values (1*M) taking from the mean of each column in LD score matrix.
 """
 
 import numpy as np
@@ -54,6 +52,10 @@ def LDAK_thin(genotype,tau):
     # print("her: ", her)
     return her
 
+def Check(pheno, gene, h2):
+    var_delta = np.var(pheno-gene)
+    print("Observed h2, Set h2: ", 1-var_delta, h2)
+
 
 def Simulated_Phenotype(n_inds = 5, n_snps = 8, tau = 0.7):
     genotype = np.random.choice([0,1,2], size=(n_inds, n_snps), replace=True)
@@ -65,12 +67,15 @@ def Simulated_Phenotype(n_inds = 5, n_snps = 8, tau = 0.7):
     environments = np.random.normal(size=n_inds)
 
     her = np.zeros((1,n_inds))
-    # her[0] = tau # GCTA model
-    her = LDAK_thin(genotype, tau) # LDAK-thin model
+
+    her[0] = tau # GCTA model
+    # her = LDAK_thin(genotype, tau) # LDAK-thin model
+
+
     h2 = np.sum(her) / her.shape[1]
-    print("h2: ", h2, her.shape[1])
     phenotype = np.dot(genetics,(h2 ** 0.5)) + np.dot(environments,((1-h2) ** 0.5))
 
+    Check(phenotype, genetics, h2) # Check if Var(Y-G) matches with 1-h2
 
     phenotype_file = pd.DataFrame({'FID': range(1,n_inds+1),'IID': range(1,n_inds+1),"Phenotype": phenotype})
     return phenotype_file
